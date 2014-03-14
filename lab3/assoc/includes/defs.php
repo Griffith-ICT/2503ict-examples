eea4f087c821b33b726642d01f93712edc9c0c8d
Jolon <j.faichney@griffith.edu.au>
2014-03-14 11:59:09 +1000

    Revert "Reformatted"
    This reverts commit c061a000312c028b6fd44e394125e50581739560.

--- lab3/assoc/includes/defs.php
+++ lab3/assoc/includes/defs.php
@@ -6,42 +6,42 @@ include "pms.php";
 
 /* Search sample data for $name or $year or $state from form. */
 function search($name, $year, $state) {
-  global $pms; 
-  
-  // Filter $pms by $name
-  if (!empty($name)) {
-    $results = array();
-    foreach ($pms as $pm) {
-      if (stripos($pm['name'], $name) !== FALSE) {
-        $results[] = $pm;
-      }
+    global $pms; 
+
+    // Filter $pms by $name
+    if (!empty($name)) {
+	$results = array();
+	foreach ($pms as $pm) {
+	    if (stripos($pm['name'], $name) !== FALSE) {
+		$results[] = $pm;
+	    }
+	}
+	$pms = $results;
     }
-    $pms = $results;
-  }
-  
-  // Filter $pms by $year
-  if (!empty($year)) {
-    $results = array();
-    foreach ($pms as $pm) {
-      if (strpos($pm['from'], $year) !== FALSE || 
-          strpos($pm['to'], $year) !== FALSE) {
-        $results[] = $pm;
-      }
+
+    // Filter $pms by $year
+    if (!empty($year)) {
+	$results = array();
+	foreach ($pms as $pm) {
+	    if (strpos($pm['from'], $year) !== FALSE || 
+	        strpos($pm['to'], $year) !== FALSE) {
+		$results[] = $pm;
+	    }
+	}
+	$pms = $results;
     }
-    $pms = $results;
-  }
-  
-  // Filter $pms by $state
-  if (!empty($state)) {
-    $results = array();
-    foreach ($pms as $pm) {
-      if (stripos($pm['state'], $state) !== FALSE) {
-        $results[] = $pm;
-      }
+
+    // Filter $pms by $state
+    if (!empty($state)) {
+	$results = array();
+	foreach ($pms as $pm) {
+	    if (stripos($pm['state'], $state) !== FALSE) {
+		$results[] = $pm;
+	    }
+	}
+	$pms = $results;
     }
-    $pms = $results;
-  }
-  
-  return $pms;
+
+    return $pms;
 }
 ?>
